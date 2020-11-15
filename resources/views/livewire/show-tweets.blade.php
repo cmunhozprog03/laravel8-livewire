49fc7f06dc9a7ede831fbacf129c72511aa56653
cmunhozprog03 <cmunhozprog03@hotmail.com>
2020-11-15 14:10:50 -0300

    pagination

--- resources/views/livewire/show-tweets.blade.php
+++ resources/views/livewire/show-tweets.blade.php
@@ -2,12 +2,24 @@
     Show tweets
 
     <br>
-    <p>{{ $message }}</p>
-    <input type="text" name="message" id="message" wire:model='message'>
+    <p>{{ $content }}</p>
+
+    <form method="post" wire:submit.prevent='create'>
+        <input type="text" name="content" id="content" wire:model='content'>
+        @error('content') {{ $message }} @enderror
+        <button type="submit">Criar Tweet</button>
+    </form>
+    
     <hr>
 
     @foreach ($tweets as $tweet)
         {{ $tweet->user->name }} - {{ $tweet->content}} <br>
         
     @endforeach
+
+    <hr>
+
+    <div> 
+        {{ $tweets->links()}}
+    </div>
 </div>
